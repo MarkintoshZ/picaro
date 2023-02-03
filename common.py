6f66eb509b1f89bf2eadcc00244461ab824fa015
Mark Zhang <bf-109g2@hotmail.com>
2023-02-03 01:36:56 -0500

    Finish drive.py

--- common.py
+++ common.py
@@ -1,44 +1,64 @@
 "Common helper functions and datatypes for picar_4wd"""
 
 from enum import Enum
+from typing import Tuple, Union
 
 import picar_4wd as fc
+
+
+__all__ = ["Command", "drive", "left_turn", "right_turn", "get_line_status"]
 
 
 class Command(Enum):
     """Driving commands"""
     FORWARD = "forward"
     BACKWARD = "backword"
-    LEFT = "left"
-    RIGHT = "right"
+    LEFT_SPIN = "left-spin"
+    LEFT_TURN = "left-turn"
+    RIGHT_SPIN = "right-spin"
+    RIGHT_TURN = "right-turn"
     STOP = "stop"
 
 
-def drive(cmd: Command, speed: int) -> None:
+def left_turn(speed: int, diff: Union[int, None] = None):
+    if not diff:
+        diff = round(speed * 0.7)
+    fc.left_front.set_power(speed - diff)
+    fc.left_rear.set_power(speed - diff)
+    fc.right_front.set_power(speed + diff)
+    fc.right_rear.set_power(speed + diff)
+
+
+def right_turn(speed: int, diff: Union[int, None] = None):
+    if not diff:
+        diff = round(speed * 0.7)
+    fc.left_front.set_power(speed + diff)
+    fc.left_rear.set_power(speed + diff)
+    fc.right_front.set_power(speed - diff)
+    fc.right_rear.set_power(speed - diff)
+
+
+def drive(cmd: Command, speed: int = 20) -> None:
     """Execute driving command by setting motor speed"""
+    if cmd == Command.STOP:
+        fc.stop()
     func = {
         Command.FORWARD: fc.forward,
         Command.BACKWARD: fc.backward,
-        Command.LEFT: fc.turn_left,
-        Command.RIGHT: fc.turn_right,
+        Command.LEFT_SPIN: fc.turn_left,
+        Command.LEFT_TURN: left_turn,
+        Command.RIGHT_SPIN: fc.turn_right,
+        Command.RIGHT_TURN: right_turn,
     }[cmd]
     func(speed)
 
 
-class LineStatus(Enum):
-    """Greyscale sensor readings"""
-    CLEAR = "clear"
-    CENTER = "center"
-    LEFT = "left"
-    RIGHT = "right"
-
-
-def line_status():
-    gs_list = fc.get_grayscale_list()
-    status = fc.get_line_status(400, gs_list)
-    if status is None:
-        return LineStatus.CLEAR
-    return [LineStatus.CENTER, LineStatus.RIGHT, LineStatus.LEFT][status]
-
-
-__all__ = [Command, drive, LineStatus, line_status]
+def get_line_status(threshold: int = 400) -> Tuple[bool, bool, bool]:
+    """Return a tuple of three bool representing whether left, middle, and 
+    right sensors are detecting dark surfaces respectively by comparing the 
+    sensor reading to the given threshold"""
+    return (
+        fc.gs2.read() < threshold,
+        fc.gs1.read() < threshold,
+        fc.gs0.read() < threshold,
+    )
--- drive.py
+++ drive.py
@@ -1,23 +1,96 @@
+import math
+from time import sleep
+from typing import List
+from queue import Queue
+from threading import Thread
+
 import picar_4wd as fc
+
 from common import *
+
 
 SPEED = 20
 
-def avoidance():
-    scan_list = None
-    while not scan_list:
-        scan_list = fc.scan_step(35)
-    tmp = scan_list[3:7]
-    print(tmp)
-    if tmp != [2, 2, 2, 2]:
-        fc.turn_right(SPEED)
-    else:
-        fc.forward(SPEED)
 
+def ultrasonic_sensor_scan(us_queue: Queue):
+    while True:
+        scan = fc.scan_step(35)
+        if scan:
+            us_queue.put(scan)
+
+
+def find_new_direction(us_queue: Queue):
+    drive(Command.RIGHT_SPIN, SPEED)
+    while True:
+        if us_queue.not_empty:
+            scan: List[int] = us_queue.get()
+            if min(scan[3:7]) == 2:
+                break
+        sleep(0.1)
+    drive(Command.STOP)
+            
 
 def main():
-    pass
+    us_queue = Queue()
+    us_thread = Thread(target=ultrasonic_sensor_scan, args=(us_queue,))
+    us_thread.start()
+
+    while True:
+        us_direction, gs_direction = 0, 0
+        
+        # get driving direction based on ultrasonic sensor
+        if us_queue.not_empty:
+            scan: List[int] = us_queue.get()
+            left = min(scan[3:6])
+            right = min(scan[5:8])
+            if left > right and left == 0:
+                us_direction = -2
+            elif left > right:
+                us_direction = -1
+            elif left < right and right == 0:
+                us_direction = 1
+            elif left < right:
+                us_direction = 2
+            elif left == 0 and right == 0:
+                find_new_direction(us_queue)
+
+        # get driving direction based on grayscale sensor
+        left, mid, right = get_line_status()
+        if left and mid:
+            gs_direction = -2
+        elif left:
+            gs_direction = -1
+        elif right and mid:
+            gs_direction = 1
+        elif right:
+            gs_direction = 2
+        elif left and mid and right:
+            drive(Command.BACKWARD, SPEED)
+            sleep(1)
+            drive(Command.STOP)
+
+        # direction negotiation
+        direction = 0
+        prod = us_direction * gs_direction
+        if prod < 0 and abs(prod) == 4:
+            find_new_direction(us_queue)
+        elif prod < 0:
+            direction = (us_direction + gs_direction) * 2
+        elif prod == 0:
+            direction = us_direction + gs_direction
+        else:
+            direction = math.floor((us_direction + gs_direction) / 2)
+
+        commands = [
+                Command.FORWARD,
+                Command.RIGHT_TURN,
+                Command.RIGHT_SPIN,
+                Command.LEFT_SPIN,
+                Command.LEFT_TURN,
+                ]
+        drive(commands[direction], SPEED)
 
 
 if __name__ == '__main__':
     main()
+
