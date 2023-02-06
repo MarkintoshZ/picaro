636e59c79dfde732b6e5371d678449a1b2a2fb24
Mark Zhang <46978338+MarkintoshZ@users.noreply.github.com>
2023-02-05 19:27:08 -0500

    Update simple_drive.py
    Simplify simple_drive.py

--- simple_drive.py
+++ simple_drive.py
@@ -1,31 +1,23 @@
 import picar_4wd as fc
 
-from common import *
-
-
-SPEED = 10
-
+speed = 30
 
 def main():
     while True:
-        if [fc.get_status_at(-15), fc.get_status_at(15)] == [2, 2]:
-            drive(Command.FORWARD, SPEED)
-        else:
-            drive(Command.STOP)
-            left_dist = fc.get_status_at(-30)
-            right_dist = fc.get_status_at(30)
-            if left_dist > right_dist:
-                if left_dist == 2:
-                    drive(Command.LEFT_TURN, SPEED)
-                else:
-                    drive(Command.LEFT_SPIN, SPEED)
+        scan_list = fc.scan_step(35)
+        if not scan_list:
+            continue
+       
+        if scan_list[3:7] != [2,2,2,2]:
+            if sum(scan_list[:3]) >= sum(scan_list[7:]):
+                fc.turn_left(speed)
             else:
-                if right_dist == 2:
-                    drive(Command.RIGHT_TURN, SPEED)
-                else:
-                    drive(Command.RIGHT_SPIN, SPEED)
+                fc.turn_right(speed)
+        else:
+            fc.forward(speed)
 
-
-if __name__ == '__main__':
-    main()
-
+if __name__ == "__main__":
+    try: 
+        main()
+    finally: 
+        fc.stop()
