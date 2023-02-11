3b02f832cd9b4c4e68b1b68541d0e7fba39ad8fa
Mark <bf-109g2@hotmail.com>
2023-02-10 20:43:17 -0600

    Mapper class

--- map.py
+++ map.py
@@ -2,12 +2,12 @@ import numpy as np
 from typing import NamedTuple, Iterable
 
 Ray = NamedTuple(
-        "Ray", 
-        [
-            ('origin', Iterable), 
-            ('angle', float), 
-            ('dist', int), 
-        ])
+    "Ray",
+    [
+        ('origin', Iterable),
+        ('angle', float),
+        ('dist', int),
+    ])
 
 
 class LocationTracker:
@@ -16,22 +16,97 @@ class LocationTracker:
 
 
 class Mapper:
-    """Maps the environent"""
+    """Maps the environment"""
 
     EMPTY = 0
     UNKNOWN = 1
     FILLED = 2
 
-    def __init__(self, size=100):
+    def __init__(self, size=100, dist_cutoff=8, connect_cutoff=5):
         self.size = size
+        self.dist_cutoff = dist_cutoff
+        self.connect_cutoff = connect_cutoff
         self.data = np.ones((size, size)) * Mapper.UNKNOWN
         self.rays = []
 
     def add_ray(self, ray: Ray):
-        x, y = ray.origin
-        self.data[x, y] = 0
-        # Todo: calculate area
+        x, y = np.array(ray.origin).round().astype(int)
+        self.data[y, x] = 0
+        if self.rays:
+            last_ray = self.rays[-1]
+            last_ray_end = (last_ray.origin[0] + np.cos(last_ray.angle) * last_ray.dist,
+                            last_ray.origin[1] + np.sin(last_ray.angle) * last_ray.dist)
+            this_ray_end = (ray.origin[0] + np.cos(ray.angle) * ray.dist,
+                            ray.origin[1] + np.sin(ray.angle) * ray.dist)
+            self.shade_triangle(last_ray.origin, ray.origin, last_ray_end)
+            self.shade_triangle(last_ray.origin, ray.origin, this_ray_end)
+            if np.linalg.norm(np.array(last_ray_end) - np.array(this_ray_end)) < self.connect_cutoff:
+                self.shade_triangle(
+                    last_ray.origin, this_ray_end, last_ray_end)
+                if max(ray.dist, last_ray.dist) < self.dist_cutoff:
+                    print(last_ray_end)
+                    self.draw_line(this_ray_end, last_ray_end)
         self.rays.append(ray)
+
+    def shade_triangle(self, p1, p2, p3):
+        """Clear the area inside the triangle defined by the three points"""
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+        p3 = np.array(p3)
+
+        # Calculate the bounding box
+        min_x = np.floor(min(p1[0], p2[0], p3[0])).astype(int)
+        max_x = np.ceil(max(p1[0], p2[0], p3[0])).astype(int)
+        min_y = np.floor(min(p1[1], p2[1], p3[1])).astype(int)
+        max_y = np.ceil(max(p1[1], p2[1], p3[1])).astype(int)
+
+        # Check each point in the bounding box
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                # Check if the point is inside the triangle
+                inside = self.is_inside_triangle(p1, p2, p3, (x, y))
+                if inside:
+                    self.data[y, x] = Mapper.EMPTY
+
+    def is_inside_triangle(self, p1, p2, p3, p):
+        """Check if a point is inside a triangle"""
+        v0 = p3 - p1
+        v1 = p2 - p1
+        v2 = p - p1
+
+        dot00 = np.dot(v0, v0)
+        dot01 = np.dot(v0, v1)
+        dot02 = np.dot(v0, v2)
+        dot11 = np.dot(v1, v1)
+        dot12 = np.dot(v1, v2)
+
+        inv_denom = 1 / (dot00 * dot11 - dot01 * dot01)
+        u = (dot11 * dot02 - dot01 * dot12) * inv_denom
+        v = (dot00 * dot12 - dot01 * dot02) * inv_denom
+
+        return (u >= 0) and (v >= 0) and (u + v < 1)
+
+    def draw_line(self, p1, p2):
+        """Draw a line between two points"""
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+
+        # Calculate the bounding box
+        min_x = np.floor(min(p1[0], p2[0])).astype(int)
+        max_x = np.ceil(max(p1[0], p2[0])).astype(int)
+        min_y = np.floor(min(p1[1], p2[1])).astype(int)
+        max_y = np.ceil(max(p1[1], p2[1])).astype(int)
+
+        # Check each point in the bounding box
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                # Check if the point is on the line
+                if self.is_on_line(p1, p2, (x, y)):
+                    self.data[y, x] = Mapper.FILLED
+
+    def is_on_line(self, p1, p2, p):
+        """Check if a point is on a line"""
+        return np.linalg.norm(np.cross(p2 - p1, p1 - p)) / np.linalg.norm(p2 - p1) < 0.5
 
     def __str__(self):
         s = ''
@@ -48,17 +123,16 @@ class Mapper:
 
     def plot(self, show=True, save_file=None):
         import matplotlib.pyplot as plt
-        
+
+        plt.figure(dpi=400)
         plt.pcolormesh(self.data, cmap='Greys', edgecolors='k', linewidth=1)
         for ray in self.rays:
             x, y = ray.origin
             dx = np.cos(ray.angle) * ray.dist
             dy = np.sin(ray.angle) * ray.dist
-            print(x, y, dx, dy)
-            plt.arrow(x, y, dx, dy, fc="k", ec="k", color='red',
-                      head_width=0.06, head_length=0.1)
+            plt.arrow(x + 0.5, y + 0.5, dx, dy, color='g', width=0.01,
+                      head_width=0.05, head_length=0.02)
 
-        plt.gca().invert_yaxis() # origin from bottom left
         plt.gca().set_aspect('equal')
 
         if save_file and isinstance(save_file, str):
@@ -68,8 +142,15 @@ class Mapper:
 
 
 if __name__ == '__main__':
-    mapper = Mapper(50)
-    mapper.add_ray(Ray((25, 25), 0, 10))
-    mapper.add_ray(Ray((25, 25), 20 / 180 * np.pi, 10))
-    mapper.plot(show=False, save_file="map0.png")
-    print(mapper)
+    mapper = Mapper(25)
+    mapper.add_ray(Ray((4, 4), np.radians(80), 10))
+    mapper.add_ray(Ray((4, 4), np.radians(70), 10))
+    mapper.add_ray(Ray((5, 5), np.radians(50), 10))
+    mapper.add_ray(Ray((5, 5), np.radians(40), 10))
+    mapper.add_ray(Ray((6, 6), np.radians(20), 6))
+    mapper.add_ray(Ray((6, 6), np.radians(10), 6))
+    mapper.add_ray(Ray((7, 7), np.radians(-10), 4))
+    mapper.add_ray(Ray((7, 7), np.radians(-20), 4))
+    mapper.add_ray(Ray((8, 8), np.radians(20), 5))
+    mapper.add_ray(Ray((8, 8), np.radians(10), 4))
+    mapper.plot()
