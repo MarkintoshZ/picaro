00a9a69921dc63c26a3dd8a98b9a135f382cfbdb
Mark <bf-109g2@hotmail.com>
2023-02-13 21:03:44 -0600

    autopilot mock

--- autopilot.py
+++ autopilot.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import time
 from typing import List, Tuple
@@ -8,15 +9,19 @@ from map import Mapper
 from common import Car
 
 
-MAP_SIZE = 200
+MAP_SIZE = 150
 
 
 def navigate(path: List[Tuple[int, int]], car: Car, object_detection: bool):
+    if object_detection:
+        from detect import run as detect
+        detector = detect("efficientdet_lite0.tflite", 0, 640, 480, 4, False)
+
     i = 0
     while i < len(path) - 1:
         curr_loc, next_loc = path[i], path[i+1]
         delta = (next_loc[0] - curr_loc[0], next_loc[1] - curr_loc[1])
-        for j in range(i, len(path)):
+        for j in range(i + 1, len(path)):
             prev_loc, _curr_loc = path[j-1], path[j]
             if _curr_loc[0] - prev_loc[0] != delta[0] or \
                     _curr_loc[1] - prev_loc[1] != delta[1]:
@@ -25,37 +30,60 @@ def navigate(path: List[Tuple[int, int]], car: Car, object_detection: bool):
         waypoint = path[i]
 
         # find direction to waypoint
-        dir_in_rad = math.atan2(waypoint[1] - curr_loc[0],
-                                waypoint[0] - curr_loc[1])
+        dir_in_rad = math.atan2(
+            waypoint[1] - curr_loc[1], waypoint[0] - curr_loc[0])
+        print(math.degrees(dir_in_rad))
         car.turn_absolute(dir_in_rad)
+        car.forward()
 
         while True:
             curr_car_pos = car.get_position().round().astype(int)
+            print(curr_car_pos, car.curr_dir)
             if curr_car_pos[0] == waypoint[0] and curr_car_pos[1] == waypoint[1]:
                 break
 
             if object_detection:
-                pass
+                has_stop_sign = next(detector)
+                if has_stop_sign:
+                    car.stop()
+                    time.sleep(3)
+                    car.forward()
             else:
-                time.sleep(0.1)
+                time.sleep(0.05)
 
-        i += 1
+        car.stop()
 
 
 def main(object_detection: bool = False):
     mapper = Mapper(size=MAP_SIZE, dist_cutoff=50, connect_cutoff=40)
-    car = Car(position=(MAP_SIZE // 2, 10),
+    car = Car(position=(12, 15),
               dir_in_rad=math.radians(90))
 
-    path: List[Tuple[int, int]] = []  # TODO: get A* path from mapper
+    path: List[Tuple[int, int]] = [
+        (12, 15),
+        (12, 80),
+        (90, 80),
+        (90, 120),
+        (115, 120),
+        (116, 120),
+    ]  # TODO: get A* path from mapper
     navigate(path, car, object_detection)
 
-    path: List[Tuple[int, int]] = []  # TODO: get A* path from mapper
-    navigate(path, car, object_detection)
+    # path: List[Tuple[int, int]] = []  # TODO: get A* path from mapper
+    # navigate(path, car, object_detection)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-o', '--objectDetection',
+        help='Use object detection',
+        required=False,
+        default=False)
+    args = parser.parse_args()
+
     try:
-        main()
+        main(args.objectDetection)
     finally:
         fc.stop()
--- detect.py
+++ detect.py
@@ -15,7 +15,6 @@
 import argparse
 import sys
 import time
-import picar_4wd as fc
 
 import cv2
 from tflite_support.task import core
@@ -88,33 +87,28 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
             category_name = category.category_name
             objects.append(category_name)
 
-        # Stop at Stop Sign
-        if "stop sign" in objects:
-            #print('stop sign')
-            fc.stop()
-        else:
-            #print('no stop')
-            fc.forward(5)
+        yield "stop sign" in objects
 
         # Draw keypoints and edges on input image
-        image = utils.visualize(image, detection_result)
+        # image = utils.visualize(image, detection_result)
 
         # Calculate the FPS
         if counter % fps_avg_frame_count == 0:
             end_time = time.time()
             fps = fps_avg_frame_count / (end_time - start_time)
             start_time = time.time()
+        print("FPS: ", fps)
 
         # Show the FPS
-        fps_text = 'FPS = {:.1f}'.format(fps)
-        text_location = (left_margin, row_size)
-        cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                    font_size, text_color, font_thickness)
+        # fps_text = 'FPS = {:.1f}'.format(fps)
+        # text_location = (left_margin, row_size)
+        # cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+        #             font_size, text_color, font_thickness)
 
         # Stop the program if the ESC key is pressed.
         if cv2.waitKey(1) == 27:
             break
-        cv2.imshow('object_detector', image)
+        # cv2.imshow('object_detector', image)
 
     cap.release()
     cv2.destroyAllWindows()
