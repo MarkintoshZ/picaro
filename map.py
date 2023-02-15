4c3efc19afdc5af384b3ac63e349e508727ba13d
Mark <bf-109g2@hotmail.com>
2023-02-14 21:36:24 -0600

    Fix typo

--- map.py
+++ map.py
@@ -157,7 +157,7 @@ class Mapper:
         blurred = gaussian_filter(obstacle_map, sigma=1)
         extruded = (blurred > 0.001).astype(int)
         import matplotlib.pyplot as plt
-        plt.polcolormesh(extruded, cmap='Greys')
+        plt.pcolormesh(extruded, cmap='Greys')
         plt.show()
         return astar(extruded.T, start, dest)
 
